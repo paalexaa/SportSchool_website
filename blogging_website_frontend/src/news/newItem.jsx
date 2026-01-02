2ae38c81ae2c579cc120b4b531c9f0414d5936ac
pozdniakova <xpozdniakova@stuba.sk>
2026-01-02 09:29:33 +0100

    deleted deploy react

--- blogging_website_frontend/src/news/newItem.jsx
+++ blogging_website_frontend/src/news/newItem.jsx
@@ -21,10 +21,6 @@ export default function NewsDetail() {
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    window.scrollTo(0, 0);
-  }, []);
-
-  useEffect(() => {
     fetchArticle();
   }, [slug]);
 
